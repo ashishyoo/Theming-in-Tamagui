caa3dc3ebf203fa91b9b25b423325d38f10c7061
Ashish Shrestha <ashishshr27@gmail.com>
2025-07-14 11:44:59 +0545

    refactor: apply theme and variant

--- packages/app/features/user/detail-screen.tsx
+++ packages/app/features/user/detail-screen.tsx
@@ -1,14 +1,17 @@
 import { Button, Paragraph, YStack } from '@my/ui'
 import { ChevronLeft } from '@tamagui/lucide-icons'
 import { useRouter } from 'solito/navigation'
+import { useVariantContext } from 'app/provider/VariantContext'
 
 export function UserDetailScreen({ id }: { id: string }) {
   const router = useRouter()
+  const { variant } = useVariantContext()
+
   if (!id) {
     return null
   }
   return (
-    <YStack flex={1} justify="center" items="center" gap="$4" bg="$background">
+    <YStack flex={1} justify="center" items="center" gap="$4" bg="$background" theme={variant}>
       <Paragraph text="center" fontWeight="700" color="$blue10">{`User ID: ${id}`}</Paragraph>
       <Button icon={ChevronLeft} onPress={() => router.back()}>
         Go Home
